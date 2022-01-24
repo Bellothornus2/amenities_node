bfb255bea466b01c3435d7f8215dc99b95384a49
bellothornus2 <divanov@cifpfbmoll.eu>
2022-01-24 18:18:27 +0100

    test(tests): :white_check_mark: changed put test and added delete test

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const { response } = require('../app');
 const app = require('../app');
 
 describe('Sample Test',() =>{
@@ -10,13 +11,24 @@ describe('Sample Test',() =>{
 describe('Changing Amenities', () =>{
     it('should change the price', async() => {
         const res = await request(app)
-        .post('amenity/getAmenityByIdAndUpdate/61daf57354e4911c13494ce0')
+        .post('/amenity/getAmenityByIdAndUpdate/61daf57354e4911c13494ce0')
         .send({
             price:25
         })
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('post')
-        expect(response.body._id).toBe("61daf57354e4911c13494ce0");
-        expect(response.body.price).toBe(25)
+        expect(res.statusCode).toEqual(302);
+        expect(res.headers.location).toContain('/amenity/getAmenityById/61daf57354e4911c13494ce0')
     })
 })
+
+describe('Deleting Amenities', () => {
+    it('should delete the Amenitiy', async() =>{
+        const constructor = await request(app)
+        .get('/amenity/factory/paquete de para eliminar/20/Hola/Soy la descripcion/true/false');
+        const idObjeto = constructor.body._id;
+        const res = await request(app)
+        .delete('/amenity/getAmenityByIdAndDelete/'+idObjeto);
+        const res2 = await request(app)
+        .get('/amenity/getAmenityById/'+idObjeto);
+        expect(res2.body).toBe(null);
+    })
+});
